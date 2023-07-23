49507d93ce576a4d71464b6fa2d45e33478ee9e9
AsadEducation <AsadEducation@outlook.com>
2023-07-23 07:05:34 +0600

    mama learned

--- test.cpp
+++ test.cpp
@@ -5,4 +5,7 @@ int main()
     cout<<"Hello world";
     cout<<"have a good day mr.";
     cout<<"learned git and github and processing";
+    cout<<"mama i hava learned git and konw how to push my code";
+    cout<<"can you wait for me";
+    return 0;
 }
